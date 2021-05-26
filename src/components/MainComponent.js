fa6a4b740840999bbd3ce66b34fc60a41dab67a5
JasonH51 <81194110+JasonH51@users.noreply.github.com>
2021-05-26 15:38:20 -0500

    contactus and Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,8 +4,9 @@ import Footer from "./FooterComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteinfoComponent";
 import Home from "./HomeComponent";
-import { Switch, Route, Redirect } from "react-router-dom";
+import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
@@ -33,12 +34,23 @@ class Main extends Component {
       );
     };
 
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={this.state.campsites.filter((campsite) => campsite.id === +match.params.campsiteId)[0]}
+          comments={this.state.comments.filter((comment) => comment.campsiteId === +match.params.campsiteId)}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+          <Route exact path="/aboutus" render={() => <About partners={this.state.partners} />} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
