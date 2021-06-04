f8bf826eea8cf42ba62c52492431903af24128c1
JasonH51 <81194110+JasonH51@users.noreply.github.com>
2021-06-04 14:44:07 -0500

    fetching data from server setup

--- src/components/CampsiteinfoComponent.js
+++ src/components/CampsiteinfoComponent.js
@@ -37,7 +37,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -56,7 +56,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </ul>
           );
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -79,7 +79,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.handleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -180,7 +180,7 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} addComment={props.addComment} campsiteId={props.campsite.id} />
+          <RenderComments comments={props.comments} postComment={props.postComment} campsiteId={props.campsite.id} />
         </div>
       </div>
     );
